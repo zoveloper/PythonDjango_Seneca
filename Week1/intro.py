6db7e618ab4b1449f395898a4c6f3cb85f82300f
Martin Cho <zoveloper@gmail.com>
2022-01-22 23:54:56 -0500

    python list and tuple and set

--- Week1/intro.py
+++ Week1/intro.py
@@ -1,3 +1,12 @@
 print("Hello World")
 student_dict={"Name":"Seneca College", "Course":"python","numStudents":11}
 student_dict["Name"]
+
+
+# A tuple is very similar to a list but instead of using square brackets you use normal brackets. The key difference between lists and tuples is that you can't modify a tuple, whereas you can add and remove which called immutable. braces instead of square or normal brackets. And the key difference between a set and the other two is that while you can add and remove elements from a set you can't have duplicate elements. So you couldn't have Kannan twice in the set. Also, lists and tuples keep the order of the elements. So whenever you print out this list, it will always have Jun then Kannan and then Kevin in that order, but sets don't necessarily keep the order,
+
+l = ["Jun","Kannan","Kevin"] #List with squre bracket /mutable
+t = ("Jun","Kannan","Kevin") #Tuple with normal bracket /immutable
+s = {"Jun","Kannan","Kevin"} #Set with curly braces
+
+
